161ee7bfd7c3d0be550651e70b922be50d00004f
ZETRO <110453029+ZETRO2@users.noreply.github.com>
2025-06-24 13:14:57 -0700

    Create 2024_J1.py

---
+++ 2024/2024_J1.py
@@ -0,0 +1,10 @@
+
+R = int(input("Enter the number of red plates (cost $3 each): "))
+
+G = int(input("Enter the number of green plates (cost $4 each): "))
+
+B = int(input("Enter the number of blue plates (cost $5 each): "))
+
+C = R * 3 + G * 4 + B * 5
+
+print("The total cost of your meal is: ${}".format(C))
